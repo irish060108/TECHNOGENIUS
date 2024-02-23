3eeefa4c4292ebb8fba8171adce4a91cdc7432fe
irish060108 <160743330+irish060108@users.noreply.github.com>
2024-02-23 08:55:59 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,93 +1,88 @@
+<?php
+session_start();
+if (isset($_SESSION['admin_id']) && 
+	isset($_SESSION['role'])) {
+
+	if ($_SESSION['role'] == 'Admin'){
+
+	
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width,
 	initial-scale=1.0">
-	<title>TECHNOGENIUS</title>
+	<title>Admin - Home</title>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/style.css">
-	<link rel="icon" href="logo.png">
+	<link rel="stylesheet" href="../css/style.css">
+	<link rel="icon" href="../logo.png">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
-<body class="body-home">
-	<div class ="black-fill"><br /><br />
-		<div class="container">
-		<nav class="navbar navbar-expand-lg bg-light"
-			 id="homeNav">
-		  <div class="container-fluid">
-		    <a class="navbar-brand" href="#">
-		    	<img src="logo.png" width="40">
-		    </a>
-		    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-		      <span class="navbar-toggler-icon"></span>
-		    </button>
-		    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-		      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-		        <li class="nav-item">
-		          <a class="nav-link active" aria-current="page" href="#">Home</a>
-		        </li>
-		        <li class="nav-item">
-		          <a class="nav-link" href="#about">About</a>
-		        </li>
-		        <li class="nav-item">
-		          <a class="nav-link" href="#contact">Contact</a>
-		        </li>
-		      </ul>
-		      <ul class="navbar-nav me-right mb-2 mb-lg-0">
-		      	<li class="nav-item">
-		          <a class="nav-link" href="login.php">Login</a>
-		        </li>
-		    </ul>
-		  </div>
-		    </div>
-		</nav>
-		<section class="welcome-text d-flex justify-content-center align-items-center flex-column">
-			<img src="logo.png">
-			<h4>Juan Basangan National High School</h4>
-			<p>Formerly San Francisco National High School</p>
-		</section>
-		<section id="about"
-				 class="d-flex justify-content-center align-items-center flex-column">
-			<div class="card mb-3 card-1">
-			  <div class="row g-0">
-			    <div class="col-md-4">
-			      <img src="logo.png" class="img-fluid rounded-start">
-			    </div>
-			    <div class="col-md-8">
-			      <div class="card-body">
-			        <h5 class="card-title">About Us</h5>
-			        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-			        <p class="card-text"><small class="text-body-secondary">Juan Basangan NHS</small></p>
-			      </div>
-			    </div>
-			  </div>
+<body>
+	<?php
+		include "inc/navbar.php";
+	?>
+	<div class="container mt-5">
+		<div class="container text-center">
+			<div class="row row-cols-5">
+				<a href="teachers.php" class="col btn btn-dark m-2 py-3">
+					<i class="fa fa-user-md fs-1" aria-hidden="true"></i><br>
+					Teachers
+				</a>
+				<a href="student.php" class="col btn btn-dark m-2 py-3">
+					<i class="fa fa-graduation-cap fs-1" aria-hidden="true"></i><br>
+					Students
+				</a>
+				<a href="registrar.php" class="col btn btn-dark m-2 py-3">
+					<i class="fa fa-pencil-square fs-1" aria-hidden="true"></i><br>
+					Registrar-Office
+				</a>
+				<a href="class.php" class="col btn btn-dark m-2 py-3">
+					<i class="fa fa-cubes fs-1" aria-hidden="true"></i><br>
+					Class
+				</a>
+				<a href="section.php" class="col btn btn-dark m-2 py-3">
+					<i class="fa fa-columns fs-1" aria-hidden="true"></i><br>
+					Section
+				</a>
+				<a href="schedule.php" class="col btn btn-dark m-2 py-3">
+					<i class="fa fa-calendar fs-1" aria-hidden="true"></i><br>
+					Schedule
+				</a>
+				<a href="message.php" class="col btn btn-dark m-2 py-3">
+					<i class="fa fa-envelope fs-1" aria-hidden="true"></i><br>
+					Message
+				</a>
+				<a href="settings.php" class="col btn btn-primary m-2 py-3 col-5">
+					<i class="fa fa-cogs fs-1" aria-hidden="true"></i><br>
+					Settings
+				</a>
+				<a href="../logout.php" class="col btn btn-warning m-2 py-3 col-5">
+					<i class="fa fa-sign-out fs-1" aria-hidden="true"></i><br>
+					Logout
+				</a>
 			</div>
-		</section>
-		<section id="contact"
-				 class="d-flex justify-content-center align-items-center flex-column">
-			<form>
-				<h3>Contact Us</h3>
-			  <div class="mb-3">
-			    <label for="exampleInputEmail1" class="form-label">Email address</label>
-			    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-			    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-			  </div>
-			  <div class="mb-3">
-			    <label class="form-label">Name</label>
-			    <input type="text" class="form-control">
-			  </div>
-			  <div class="mb-3">
-			    <label class="form-label">Message</label>
-			    <textarea class="form-control" rows="4"></textarea>
-			  </div>
-			  <button type="submit" class="btn btn-primary">Send</button>
-			</form>
-		</section>
-		<div class="text-center text-light">
-			Copyright &copy; 2023 Juan Basangan National High School. All rights reserved.
-		</div>
 		</div>
 	</div>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js">
+	</script>
+	<script>
+		$(document).ready(function(){
+			$("#navLinks li:nth-child(1) a").addClass('active');
+		});
+
+	</script>
 </body>
 </html>
+<?php 
+	}else {
+	header("Location: ../login.php");
+	exit;
+} 
+}else {
+	header("Location: ../login.php");
+	exit;
+} 
+ ?>
